cf3de784a86540e8753f849c8d8069f7a833e702
JeseungHwang <hjs0790@gmail.com>
2017-02-16 20:30:26 +0900

    test

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
-var express = require('express');
+var express = require('express');   
 var path = require('path');
-var app = express();						
-//var bodyParser = require('body-parser');	//
+var app = express();				        //서버 생성		
+var bodyParser = require('body-parser');	//
 var session = require('express-session');	//세션 관련
 var fs = require("fs");						//JSON 파일
 var mysql = require('mysql');				//mysql 관련
 var http = require('http');					//HTTP 통신 관련
  
 app.use(express.static(path.join( __dirname + '/public')));
-//app.use(express.static('public'));
+app.use(bodyParser.json());
+//app.use(bodyParser.urlencoded({ extended: true })); // url인코딩 관련
+
  
 var server = app.listen(3000, function(){
  console.log("Express server has started on port 3000")
@@ -35,3 +37,4 @@ connection.connect(function(err) {
 //REST 요청에 따른 Router 등록
 var r_application = require('./router/application_router')(app, fs, mysql, connection);
 var r_switch = require('./router/switch_router')(app, fs, http);
+var r_logging = require('./router/logging_router')(app, fs, http);
