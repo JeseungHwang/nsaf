8e4fc511f7747a9ba99014d13934c6da849ab986
JeseungHwang <hjs0790@gmail.com>
2017-02-28 21:23:00 +0900

    Switch 리스트 불러오고 뿌리기 작업

--- router/logging_router.js
+++ router/logging_router.js
@@ -6,6 +6,17 @@ module.exports = function(app, fs, http, Log)
 		return moment().format('YYYY-MM-DD hh:mm:ss');
 	}
 
+	app.get('/log', function(req,res){
+		Log.find(function(err, data){
+			if(err){ return res.status(500).send({error: 'database failure'}); }
+        	else{
+        		res.json(data);
+        	} 
+		}).sort({"timestamp" : -1})
+		//DB에서 가져온 쿼리 결과값을 timestamp 내림차순 설정
+
+	});
+
 	app.post('/log', function(req,res){
 		var log = new Log();
         var result='';
@@ -14,14 +25,14 @@ module.exports = function(app, fs, http, Log)
 	    log.logtype = 'INFO';
 	    log.subject = 'test01';
 	    log.message = req.body.log;
-
+	    //console.log(log);
+	    res.json({result: 1});	
         /*log.save(function(err){
 			if(err){
 				console.error(err);
 				res.json({result: 0});
 				return;
 	        }else{
-	        	console.log("success");
 	        	res.json({result: 1});	
 	        }
 	    });*/
--- router/switch_router.js
+++ router/switch_router.js
@@ -1,6 +1,5 @@
 module.exports = function(app, fs, http)
 {
-	//
 	var basicOptions = {
 	    hostname: '192.168.56.104',
 	    port: '8080',
@@ -9,7 +8,7 @@ module.exports = function(app, fs, http)
 	    }
 	};
 
-	app.get('/switch', function(req,res){
+	/*app.get('/switch', function(req,res){
         var result='';
         var restOptions = basicOptions;
         restOptions['method'] = 'GET';
@@ -22,6 +21,52 @@ module.exports = function(app, fs, http)
 			});
 			response.on('end', function () {
 			    res.json(JSON.parse(str));
+			});
+		}).end();
+    });*/
+
+	app.get('/switch', function(req,res){
+        var result = [];
+        var restOptions = basicOptions;
+        restOptions['method'] = 'GET';
+        restOptions['path'] = '/wm/core/switch/all/desc/json';
+
+        http.request(restOptions, function(response){
+			var str = '';
+			response.on('data', function (chunk) {
+				str += chunk;
+			});
+			response.on('end', function () {
+				result.push(JSON.parse(str));
+				
+				restOptions['path'] = '/wm/core/switch/all/port-desc/json';
+				http.request(restOptions, function(response){
+					var str = '';
+					response.on('data', function (chunk) {
+						str += chunk;
+					});
+					response.on('end', function () {
+						result.push(JSON.parse(str));
+						
+						restOptions['path'] = '/wm/core/controller/switches/json';
+						http.request(restOptions, function(response){
+							var str = '';
+							response.on('data', function (chunk) {
+								str += chunk;
+							});
+							response.on('end', function () {
+								var res_data = JSON.parse(str);
+								var jsonObj = {};
+								for(var i=0; i<res_data.length;i++){
+									var obj_name = res_data[i].switchDPID.substring(0,25);
+									jsonObj[obj_name] = res_data[i];
+								}
+								result.push(jsonObj);
+							    res.json(result);
+							});
+						}).end();
+					});
+				}).end();				
 			});
 		}).end();
     });
